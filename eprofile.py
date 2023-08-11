9d7fa2349a3f1ff143b8a4426139e9b48aab758b
Nehemiah Monrose <139259756+NMonKLabs77@users.noreply.github.com>
2023-08-11 03:37:28 -0400

    Update eprofile.py

--- eprofile.py
+++ eprofile.py
@@ -10,37 +10,15 @@ class employee_profile:
       self.id = code
       print(f'First Name: ',  self.fname, 'Last Name: ', self.lname, 'Job role: ', self.job, 'ID: ', self.id)
 
+EP1 = employee_profile("Nehemiah", "Monrose", "Junior Developer", "123456")
+print(EP1)
 
-fname = input('Enter first name:')
-lname = input('Enter last name: ')
-job = input('Enter job role:')
-code = input('Enter job code:')
-
-EP = employee_profile(fname, lname, job, code)
-print(EP)
-#Create child class income_profile of parent class
-class income_profile:
-
-  def __init__(self,hours, hourly_rate ):
-    self.hours = hours
-    self. hourly_rate = hourly_rate
-    
-  def total_income(self): 
-    #Used If statements to calculate overtime and normal time payments 
-    if int(self.hours) <= 40:
-       print("Your payment this week is: ", float(self.hours) * float(self.hourly_rate))
-    elif int(self.hours) > 40:
-       print("Your over-time payment this week is: ", (float(self.hourly_rate) * 2.5) * float(self.hours))
-    elif int(self.hours) == 0:
-       print("NO PAYMENT THIS WEEK")
-  
-    
-   
-hours = input('Enter your weekly hours: ')
-hourly_rate = input('Enter the hourly rate: ')
-
-
-
-
-my_income = income_profile(hours, hourly_rate)
-my_income.total_income()
+subjects = ["Mathematics",  "English"]
+for x in subjects:
+    print(x.upper())
+    if "English" in x:
+        print("There are a million words I can think of that stats with the letter A")
+    elif "Geography" in x:
+        print("Tectonic plates ")
+    if "Mathematics" in x:
+        print(28890 * 6789 / (145 * 23))
